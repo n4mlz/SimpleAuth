f560a244bea5fc02db3e56d512f934eb692500e7
Nameless-itf23 <ohutoncat@gmail.com>
2023-10-25 20:40:15 +0900

    made setprofile form

--- src/components/EmailVerification.tsx
+++ src/components/EmailVerification.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { sendEmailVerification } from "firebase/auth";
-import { doc, setDoc, getDoc, Timestamp } from "firebase/firestore"; 
+import { doc, setDoc, getDoc, Timestamp } from "firebase/firestore";
 import { db, SignOut } from "../firebase";
 import { useAuthContext } from "../contexts/AuthContext";
 
@@ -10,42 +10,44 @@ const EmailVerification: React.FC = () => {
     const [isSentEmail, setIsSentEmail] = useState<boolean>(true);
 
     useEffect(() => {
-        if (user) { (async () => {
-            const mailsentRef = doc(db, "mailsent", String(user?.uid));
+        if (user) {
+            (async () => {
+                const mailsentRef = doc(db, "mailsent", String(user?.uid));
 
-            const checkDb = async () => {
-                try {
-                    const time: Timestamp = (await getDoc(mailsentRef)).data()?.time;
-                    if (time) {
-                        return (Timestamp.now().seconds - time.seconds > 300);
-                    } else {
-                        return true;
+                const checkDb = async () => {
+                    try {
+                        const time: Timestamp = (await getDoc(mailsentRef)).data()?.time;
+                        if (time) {
+                            return (Timestamp.now().seconds - time.seconds > 300);
+                        } else {
+                            return true;
+                        }
+                    } catch (error) {
+                        console.log(error);
+                        return false;
                     }
-                } catch (error) {
-                    console.log(error);
-                    return false;
                 }
-            }
 
-            if (await checkDb()) {
-                setIsSentEmail(false);
-                sendEmailVerification(user);
-                setDoc(mailsentRef, {
-                    userId: user?.uid,
-                    time: Timestamp.now()
-                });
-            } else {
-                setIsSentEmail(true);
-            }
-        })()}
+                if (await checkDb()) {
+                    setIsSentEmail(false);
+                    sendEmailVerification(user);
+                    setDoc(mailsentRef, {
+                        userId: user?.uid,
+                        time: Timestamp.now()
+                    });
+                } else {
+                    setIsSentEmail(true);
+                }
+            })()
+        }
     }, [user]);
 
     return (
         <div>
             <h1>メールアドレスの確認</h1>
             <p>{isSentEmail
-                ?`5分以内にあなたのメールアドレス${user?.email}宛てに確認メールを送信済みです。メールを再送する場合は、しばらく待ってからこのページに再アクセスしてください。`
-                :`あなたのメールアドレス${user?.email}宛てに確認メールを送信しました。メール内のリンクをクリックして、アドレスが正しいことを確認してください。`}</p>
+                ? `5分以内にあなたのメールアドレス${user?.email}宛てに確認メールを送信済みです。メールを再送する場合は、しばらく待ってからこのページに再アクセスしてください。`
+                : `あなたのメールアドレス${user?.email}宛てに確認メールを送信しました。メール内のリンクをクリックして、アドレスが正しいことを確認してください。`}</p>
             <button onClick={SignOut}>サインアウト</button>
             <a href="/changeprofile">次へ</a>
         </div>
--- src/components/SetProfile.tsx
+++ src/components/SetProfile.tsx
@@ -1,18 +1,75 @@
-import React, { useEffect } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../contexts/AuthContext";
+import defaultIcon from "../assets/default-icon.png"
+
+const genderList = [
+    { name: "男性", value: "male" },
+    { name: "女性", value: "female" },
+    { name: "回答しない", value: "custom" }
+];
 
 const SetProfile: React.FC = () => {
+
     const user = useAuthContext();
     const navigate = useNavigate();
+
+    const [username, setUsername] = useState<string | undefined>(undefined);
+    const [iconFile, setIconFile] = useState<File | undefined>(undefined);
+    const [iconURL, setIconURL] = useState<string>(defaultIcon);
+    const [birth, setBirth] = useState<string | undefined>(undefined);
+    const [gender, setGender] = useState<string | undefined>(undefined);
+
+    const [isError, setIsError] = useState<boolean>(false);
+
+
+    const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files) return;
+
+        const fileObject = e.target.files[0];
+        setIconFile(fileObject);
+        setIconURL(window.URL.createObjectURL(fileObject));
+    };
+
+    const sendProfile = (event: FormEvent) => {
+        event.preventDefault();
+        if (username && iconFile && birth && gender) {
+            console.log('set');
+        } else {
+            setIsError(true);
+        }
+    }
+
     useEffect(() => {
         if (user) {
-            user.emailVerified?null:navigate('/verify');
+            user.emailVerified ? null : navigate('/verify');
         } else {
             navigate("/signin");
         };
     }, [user])
-    return (<div></div>)
+
+    return (
+        <div>
+            <h1>プロフィールを編集</h1>
+            <form onSubmit={sendProfile}>
+                <input placeholder="ユーザー名" onChange={(event) => setUsername(event.target.value)} />
+                {isError && username == undefined && <p>ユーザー名を入力してください。</p>}
+                <img src={iconURL} />
+                <input type="file" accept="image/*" onChange={onFileInputChange} />
+                {isError && iconFile == undefined && <p>アイコンを設定してください。</p>}
+                <input type="date" placeholder="生年月日" onChange={(event) => setBirth(event.target.value)} />
+                {isError && birth == undefined && <p>生年月日を入力してください。</p>}
+                {genderList.map((element) => (
+                    <label key={element.value}>
+                        <input type="radio" name="gender" value={element.value} onChange={(event) => setGender(event.target.value)} />
+                        {element.name}
+                    </label>
+                ))}
+                {isError && gender == undefined && <p>性別を入力してください。</p>}
+                <button type="submit">保存</button>
+            </form>
+        </div>
+    )
 }
 
 export default SetProfile;
